49348e21e7d18d97ed754a8f13fb6b0e4de788be
Michal Mrzena <michal.mrzena@gmail.com>
2018-02-28 21:34:51 +0100

    commit s sipkami a jejich animaci

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logoSmall = document.getElementById('logoSmall');
 var main = document.getElementById('main');
 var content = document.getElementById('content');
 var checkboxMenu = document.getElementById('checkboxMenu');
+var content = document.getElementById('content');
 
 function fadeEffect() {
 
@@ -44,4 +45,31 @@ function closeMenu() {
 //funcke na presmerovani po kliknuti na male logo na stranku o omne
 function goHome() {
   location.href = "omne.html";
+}
+
+//tohle je, kdyz je pri nacteni dana sirka < 930
+if(window.innerWidth < 930) {
+    window.addEventListener('scroll', scrollAndHide, false);
+}
+//tahle funkce udela
+function scrollAndHide() {
+  navbar.style.animationDuration = "0.3s";
+
+  if(window.oldScroll > (window.scrollY || window.pageYOffset)) {
+    navbar.className = "animated fadeIn";
+  } else {
+    navbar.className = "animated fadeOut";
+  }
+  window.oldScroll = ( window.scrollY || window.pageYOffset);
+}
+
+window.addEventListener('resize', mediaWidth, false);
+
+function mediaWidth() {
+      var width = window.innerWidth;
+      if (width < 930) {
+        window.addEventListener('scroll', scrollAndHide, false);
+      } else {
+        window.removeEventListener('scroll', scrollAndHide, false);
+      }
 }
